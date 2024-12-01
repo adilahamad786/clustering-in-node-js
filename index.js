d896b9c9970c27dd5d6918462368e4d0e8871877
= <=>
2024-12-01 23:13:36 +0530

    explain main thread blocking issue in nodejs.

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+function doWork(duration) {
+    const start = Date.now();
+    while(Date.now() - start < duration);
+}
+
 app.get('/', (req, res) => {
+    doWork(5000) // block main thread for 5s
     res.send("Hi there!");
 });
 
