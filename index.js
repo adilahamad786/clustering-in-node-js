215a5f4670a5c0ca1b59d24aa61d2206797e20a6
= <=>
2024-12-03 23:51:21 +0530

    create a basic route for testing cluster in node.

--- index.js
+++ index.js
@@ -1,24 +1,40 @@
 const cluster = require('cluster');
+const os = require('os');
 
 // Initially file execute in master mode and create an instance which is known as Cluster Master
 if (cluster.isMaster) {
+    console.log("Length : ", os.cpus().length);
+    cluster.fork(); // run again full code fore creating an insatance of worker/slave/child mode (instance).
+    cluster.fork(); // run again full code fore creating an insatance of worker/slave/child mode (instance).
+    cluster.fork(); // run again full code fore creating an insatance of worker/slave/child mode (instance).
     cluster.fork(); // run again full code fore creating an insatance of worker/slave/child mode (instance).
 }
 else {
     // this code run for child/worker/slave mode only and work as server
     const express = require('express');
+    const crypto = require('crypto');
+    const start = Date.now();
     
     const app = express();
     const port = process.env.PORT || 3000;
     
-    function doWork(duration) {
-        const start = Date.now();
-        while(Date.now() - start < duration);
+    function doHash() {
+        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+            console.log("Hash : ", Date.now() - start);
+        })
     }
     
     app.get('/', (req, res) => {
-        doWork(5000) // block main thread for 5s
+        doHash() // block main thread
+        for(let i=0; i < 10000000000; i++);
         res.send("Hi there!");
+        console.log(process.pid)
+    });
+
+    app.get('/fast', (req, res) => {
+        res.send("Fast response!");
+        console.log(process.pid)
+
     });
     
     app.listen(port, () => {
