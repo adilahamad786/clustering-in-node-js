0148d11bb3ff379eccf58590b16a3367aa3bd921
adilahamad786 <adilahamad.personally@gmail.com>
2025-01-05 12:21:34 +0530

    Undustanding parallel working of cluster in case of blocking task of plain javascript.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const cluster = require('cluster');
 const os = require('os');
+const gTime = Date.now();
 
 // Initially file execute in master mode and create an instance which is known as Cluster Master
 if (cluster.isMaster) {
@@ -13,22 +14,42 @@ else {
     // this code run for child/worker/slave mode only and work as server
     const express = require('express');
     const crypto = require('crypto');
-    const start = Date.now();
+    // const start = Date.now();
+    var start = Date.now();
+    var firstTime = true;
     
     const app = express();
     const port = process.env.PORT || 3000;
     
     function doHash() {
-        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+        crypto.pbkdf2('abcdefghijklmnopqrstuvwxyz', 'abcdefghijklmnopqrstuvwxyz', 21474836, 512, 'sha512', () => {
             console.log("Hash : ", Date.now() - start);
         })
     }
     
     app.get('/', (req, res) => {
-        doHash() // block main thread
-        for(let i=0; i < 10000000000; i++);
+        // doHash() // block main thread
+
+        console.log("Start : ", new Date(), process.pid)
+
+        // if (firstTime) {
+        //     start = Date.now();
+        //     firstTime = false;
+        // }
+
+        // const start = new Date().getTime();
+
+        for (let i = 0; i < 1000000000; i++) {
+            // do nothing
+        }
+
+        // console.log((Date.now() - start), process.pid);
+        // console.log("gTime : ", (gTime - start), process.pid);
+
+        console.log("End : ", new Date(), process.pid)
+
         res.send("Hi there!");
-        console.log(process.pid)
+        // console.log("Pid", process.pid)
     });
 
     app.get('/fast', (req, res) => {
